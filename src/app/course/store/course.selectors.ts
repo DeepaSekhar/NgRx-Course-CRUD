049e97a4ffb06c55381ac366e3e45308cbf53f13
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-09 22:44:12 +0100

    added selectors

---
+++ src/app/course/store/course.selectors.ts
@@ -0,0 +1,14 @@
+import { CourseState } from '../store/course.reducers'
+import { Course } from '../model/course.model'
+import { createSelector, createFeatureSelector } from '@ngrx/store'
+import { selectAll, selectIds } from '../store/course.reducers'
+
+export const CourseFeatureSelector = createFeatureSelector<CourseState>('courses')
+
+export const getAllCourses = createSelector(
+    CourseFeatureSelector, selectAll
+);
+export const areCourseLoaded = createSelector(
+    CourseFeatureSelector,
+    state => state.coursesLoaded
+)
