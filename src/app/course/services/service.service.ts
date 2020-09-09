c9591366ca00490af90700b1a7430b111cb2f5cf
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-09 14:18:20 +0100

    added service for the CRUD

---
+++ src/app/course/services/service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Course } from '../model/course.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  courseURL = "http://localhost:3000/course"
+  constructor(private http: HttpClient) { }
+  //get all the course
+  getAllCouse(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.courseURL)
+  }
+  //create a course
+  createCourse(course: Course): Observable<Course> {
+    return this.http.post<Course>(this.courseURL, course)
+  }
+  //delete a couse
+  deleteCourse(courseId: string): Observable<Course> {
+    return this.http.delete<Course>(this.courseURL + courseId)
+  }
+  //update a course
+  updateCourse(courseId: number | string, updates: Partial<Course>) {
+    return this.http.put(this.courseURL + courseId, updates)
+  }
+}
