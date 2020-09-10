923a53e5af36c71422b84639942711944bff5887
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-10 21:46:24 +0100

    createformfunctions

--- src/app/course/components/create-course/create-course.component.ts
+++ src/app/course/components/create-course/create-course.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { getAllCourses } from '../../../course/store/course.selectors'
-import { CourseActionTypes } from '../../../course/store/course.action'
+import { Course } from '../../../course/model/course.model'
+import { createCourse } from '../../../course/store/course.action'
+import * as uuid from 'uuid'
+import { AppState } from '../../../store/reducers';
+import { Store } from '@ngrx/store'
 @Component({
   selector: 'app-create-course',
   templateUrl: './create-course.component.html',
@@ -8,9 +11,22 @@ import { CourseActionTypes } from '../../../course/store/course.action'
 })
 export class CreateCourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
   }
 
+
+  onSubmit(submittedForm) {
+    console.log(submittedForm.value)
+    if (submittedForm.invalid) {
+      return
+    }
+    const course: Course = {
+      id: uuid.v4(),
+      name: submittedForm.value.name,
+      description: submittedForm.value.description
+    }
+    this.store.dispatch(createCourse({ course }))
+  }
 }
