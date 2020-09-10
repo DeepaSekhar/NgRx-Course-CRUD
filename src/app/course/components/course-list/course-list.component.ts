8cdf898a778e8ddf4cb0d16d2f1af81bcb00c727
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-10 19:40:39 +0100

    course ;ist component

--- src/app/course/components/course-list/course-list.component.ts
+++ src/app/course/components/course-list/course-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { getAllCourses } from '../../../course/store/course.selectors'
+import { CourseActionTypes } from '../../../course/store/course.action'
+import { AppState } from '../../../store/reducers'
+import { Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { Course } from '../../model/course.model'
+import { CourseService } from '../../services/course.service'
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms'
+import { Update } from '@ngrx/entity'
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
+  courses$: Observable<Course[]>
+  courseToBeUpdated: Course;
+  isUpdateActivated = false;
 
-  constructor() { }
+  constructor(private courseServe: CourseService, private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.courses$ = this.store.select(getAllCourses);
   }
-
+  showUpdateForm(course: Course) {
+    this.courseToBeUpdated = { ...course };
+    this.isUpdateActivated = true;
+  }
+  deleteCourse(courseId: string) {
+    this.store.dispatch(CourseActionTypes.deleteCourse({ courseId }))
+  }
 }
