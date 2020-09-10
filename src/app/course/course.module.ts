d3d824d1c38b17392f7ee466d847883dc3a2202f
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-10 13:33:18 +0100

    created components

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -5,19 +5,22 @@ import { CourseService } from '../course/services/course.service'
 import { StoreModule } from '@ngrx/store'
 import { FormsModule } from '@angular/forms'
 import { courseReducer } from './store/course.reducers'
-import { EffectsModule } from '@ngrx/effects'
+import { EffectsModule } from '@ngrx/effects';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { CreateCourseComponent } from './components/create-course/create-course.component'
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CourseListComponent, CreateCourseComponent],
   imports: [
     CommonModule,
     FormsModule,
     StoreModule.forFeature('courses', courseReducer),
     EffectsModule.forFeature([CourseEffects])
   ],
-  providers: [CourseModule]
+  providers: [CourseService],
+  exports: [CourseListComponent, CreateCourseComponent]
 })
 export class CourseModule { }
