91819d9608589a975bfe1732a6aca601facbbdd4
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-10 13:26:50 +0100

    updated module

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CourseEffects } from './store/course.effect'
 import { CommonModule } from '@angular/common';
 import { CourseService } from '../course/services/course.service'
+import { StoreModule } from '@ngrx/store'
+import { FormsModule } from '@angular/forms'
+import { courseReducer } from './store/course.reducers'
+import { EffectsModule } from '@ngrx/effects'
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    StoreModule.forFeature('courses', courseReducer),
+    EffectsModule.forFeature([CourseEffects])
   ],
   providers: [CourseModule]
 })
